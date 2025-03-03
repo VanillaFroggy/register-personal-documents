ce3b840385148fa6ab322b57d03d94eda482420f
FrolovIV <FrolovIV@git.mail.com>
2025-03-03 16:42:23 +0300

    register bugfix

--- src/main/java/com/internship/service/impl/AuthServiceImpl.java
+++ src/main/java/com/internship/service/impl/AuthServiceImpl.java
@@ -1,14 +1,14 @@
 package com.internship.service.impl;
 
+import com.internship.persistence.entity.Document;
+import com.internship.persistence.entity.DocumentGroup;
 import com.internship.persistence.entity.User;
+import com.internship.persistence.repo.DocumentGroupRepository;
+import com.internship.persistence.repo.DocumentRepository;
 import com.internship.persistence.repo.DocumentTypeRepository;
 import com.internship.persistence.repo.UserRepository;
 import com.internship.service.AuthService;
-import com.internship.service.DocumentGroupService;
-import com.internship.service.DocumentService;
 import com.internship.service.dto.auth.RegisterDto;
-import com.internship.service.dto.document.CreateDocumentDto;
-import com.internship.service.dto.group.CreateDocumentGroupDto;
 import com.internship.service.mapper.ServiceMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -26,9 +26,9 @@ import java.time.ZonedDateTime;
 public class AuthServiceImpl implements AuthService {
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
+    private final DocumentGroupRepository documentGroupRepository;
     private final DocumentTypeRepository typeRepository;
-    private final DocumentService documentService;
-    private final DocumentGroupService documentGroupService;
+    private final DocumentRepository documentRepository;
     private final ServiceMapper mapper;
 
     @Override
@@ -39,17 +39,22 @@ public class AuthServiceImpl implements AuthService {
         User user = mapper.toEntity(dto);
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
-        documentService.addDocument(new CreateDocumentDto(
-                "Пароль",
-                typeRepository.findByName("Пароль")
-                        .orElseThrow(NullPointerException::new)
-                        .getId(),
-                documentGroupService.addGroup(new CreateDocumentGroupDto(
-                        "All",
-                        "#FFFFFF"
-                )).id(),
-                ZonedDateTime.now(ZoneOffset.UTC).plusDays(60)
-        ));
+        DocumentGroup documentGroup = DocumentGroup.builder()
+                .name("All")
+                .color("#FFFFFF")
+                .user(user)
+                .build();
+        documentGroupRepository.save(documentGroup);
+        Document document = Document.builder()
+                .title("Пароль")
+                .documentGroup(documentGroup)
+                .documentType(typeRepository.findByName("Пароль")
+                        .orElseThrow(NullPointerException::new))
+                .user(user)
+                .dateOfIssue(ZonedDateTime.now(ZoneOffset.UTC))
+                .expirationDate(ZonedDateTime.now(ZoneOffset.UTC).plusDays(60))
+                .build();
+        documentRepository.save(document);
     }
 
     @Override
