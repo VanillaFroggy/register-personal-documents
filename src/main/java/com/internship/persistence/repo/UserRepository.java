bcae0c06a25296c8d257b238bf3909963bb4f363
FrolovIV <FrolovIV@git.mail.com>
2025-03-06 17:32:04 +0300

    UserRepository bean fix

--- src/main/java/com/internship/persistence/repo/UserRepository.java
+++ src/main/java/com/internship/persistence/repo/UserRepository.java
@@ -2,9 +2,11 @@ package com.internship.persistence.repo;
 
 import com.internship.persistence.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 }
