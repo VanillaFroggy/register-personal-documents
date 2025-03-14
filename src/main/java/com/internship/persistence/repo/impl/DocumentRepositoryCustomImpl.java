4e831366dc99c900d46adbfd24f6cabf112b4965
FrolovIV <FrolovIV@git.mail.com>
2025-03-14 17:36:56 +0300

    total count of rows by condition is calculated with second query correct now

--- src/main/java/com/internship/persistence/repo/impl/DocumentRepositoryCustomImpl.java
+++ src/main/java/com/internship/persistence/repo/impl/DocumentRepositoryCustomImpl.java
@@ -3,6 +3,7 @@ package com.internship.persistence.repo.impl;
 import com.internship.persistence.entity.Document;
 import com.internship.persistence.entity.QDocument;
 import com.internship.persistence.repo.DocumentRepositoryCustom;
+import com.querydsl.core.Fetchable;
 import com.querydsl.jpa.impl.JPAQuery;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
@@ -11,36 +12,47 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 @RequiredArgsConstructor
 public class DocumentRepositoryCustomImpl implements DocumentRepositoryCustom {
-    private final EntityManager em;
+    private final EntityManager entityManager;
 
     @Override
     public Page<Document> findAllByUserId(Long userId, Pageable pageable) {
-        List<Document> documents = new JPAQuery<Document>(em)
-                .select(QDocument.document)
-                .from(QDocument.document)
-                .where(QDocument.document.user.id.eq(userId))
-                .limit(pageable.getPageSize())
-                .offset(pageable.getOffset())
-                .fetch();
-        return new PageImpl<>(documents, pageable, documents.size());
+        return new PageImpl<>(
+                new JPAQuery<Document>(entityManager)
+                        .select(QDocument.document)
+                        .from(QDocument.document)
+                        .where(QDocument.document.user.id.eq(userId))
+                        .limit(pageable.getPageSize())
+                        .offset(pageable.getOffset())
+                        .fetch(),
+                pageable,
+                new JPAQuery<Document>(entityManager)
+                        .from(QDocument.document)
+                        .where(QDocument.document.user.id.eq(userId))
+                        .transform(Fetchable::fetchCount)
+        );
     }
 
     @Override
     public Page<Document> findAllByUserIdAndDocumentGroupId(Long userId, Long documentGroupId, Pageable pageable) {
-        List<Document> documents = new JPAQuery<Document>(em)
-                .select(QDocument.document)
-                .from(QDocument.document)
-                .where(QDocument.document.user.id.eq(userId)
-                        .and(QDocument.document.documentGroup.id.eq(documentGroupId)))
-                .limit(pageable.getPageSize())
-                .offset(pageable.getOffset())
-                .orderBy(QDocument.document.dateOfIssue.desc())
-                .fetch();
-        return new PageImpl<>(documents, pageable, documents.size());
+        return new PageImpl<>(
+                new JPAQuery<Document>(entityManager)
+                        .select(QDocument.document)
+                        .from(QDocument.document)
+                        .where(QDocument.document.user.id.eq(userId)
+                                .and(QDocument.document.documentGroup.id.eq(documentGroupId)))
+                        .limit(pageable.getPageSize())
+                        .offset(pageable.getOffset())
+                        .orderBy(QDocument.document.dateOfIssue.desc())
+                        .fetch(),
+                pageable,
+                new JPAQuery<Document>(entityManager)
+                        .from(QDocument.document)
+                        .where(QDocument.document.user.id.eq(userId)
+                                .and(QDocument.document.documentGroup.id.eq(documentGroupId)))
+                        .transform(Fetchable::fetchCount)
+        );
     }
 }
